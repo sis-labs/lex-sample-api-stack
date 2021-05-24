8ae31cda54058e87b76fa8011a660bfce398aa4e
ollie314 <mehdi.lefebvre@gmail.com>
2021-05-24 05:30:13 +0200

    feat: add base for appointment api
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

--- packages/appointment-api/lib/appointment-api.js
+++ packages/appointment-api/lib/appointment-api.js
@@ -1,7 +1,28 @@
 'use strict';
 
-module.exports = appointmentApi;
+require('dotenv').config();
+const fastify = require('fastify')({logger: true});
 
-function appointmentApi() {
-    // TODO
-}
+const { getStartFunction, createHealthCheckRoute } = require('api-common');
+const { now, searchAppointments } = require('./domain');
+const { HOST, PORT } = process.env;
+
+const start = getStartFunction(fastify,  HOST, PORT);
+
+createHealthCheckRoute(fastify);
+
+// Search for appointment
+fastify.get('/appointments', async (request, reply) => {
+    reply.code(200)
+	.header('content-type', 'application/json;charset=utf-8')
+	.send({code: 0, message: 'ok'});
+});
+
+// Create a new appointment
+fastify.post('/appointments', async (request, reply) => {
+    reply.code(200)
+	.header('content-type', 'application/json;charset=utf-8')
+	.send({code: 0, message: 'ok'});
+});
+
+start();
---
+++ packages/appointment-api/lib/domain.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    now,
+    searchAppointments,
+};
+
+function now() {
+    return Date.now();
+}
+
+function searchAppointments(starDate = null, endDate = null) {
+    return [];
+}
